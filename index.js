bec1ca9e5e2c8be01645878132ea9bd50e3c721c
perqin <perqinxie@gmail.com>
2017-06-16 15:01:01 +0800

    Add log

--- index.js
+++ index.js
@@ -8,4 +8,12 @@ hexo.config.hexo_showcase = assign({
     showcase_dir: 'showcase'
 }, hexo.config.hexo_showcase);
 
+// hexo.extend.console.register('showcase', 'Create Showcase post', function (args) {
+//     const self = this;
+//     args.p = 'showcase'
+//     return self.extend.console.get('new').call(self, {
+//         //
+//     });
+// });
+
 hexo.extend.generator.register('showcase', require('./lib/generator'));
