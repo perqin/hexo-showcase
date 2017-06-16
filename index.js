f1288384eb4d2e73a750fe53b6f11508f7996df6
perqin <perqinxie@gmail.com>
2017-06-16 15:50:36 +0800

    Update console command

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const assign = require('object-assign');
+var tildify = require('tildify');
 
 hexo.config.hexo_showcase = assign({
     showcase_dir: 'showcase'
@@ -24,14 +25,54 @@ hexo.scaffold.get('showcase', (err) => {
 });
 
 hexo.extend.console.register('showcase', 'Create Showcase post', function (args) {
+    // // Display help message if user didn't input any arguments
+    // if (!args._.length) {
+    //     return this.call('help', {_: ['showcase']});
+    // }
+    //
+    // const self = this;
+    // if (args._.length) args._[0] = 'showcase';
+    // return self.extend.console.get('new').call(self, args);
+
+
+    var reservedKeys = {
+        _: true,
+        title: true,
+        layout: true,
+        slug: true,
+        path: true,
+        replace: true,
+        // Global options
+        config: true,
+        debug: true,
+        safe: true,
+        silent: true
+    };
+
     // Display help message if user didn't input any arguments
     if (!args._.length) {
-        return this.call('help', {_: ['showcase']});
+        return this.call('help', {_: ['new']});
     }
 
-    const self = this;
-    if (args._.length) args._[0] = 'showcase';
-    return self.extend.console.get('new').call(self, args);
+    var data = {
+        title: args._.pop(),
+        layout: 'showcase',
+        slug: args.s || args.slug
+    };
+    data.path = 'showcase/' + data.title;
+
+    var keys = Object.keys(args);
+    var key = '';
+    var self = this;
+
+    for (var i = 0, len = keys.length; i < len; i++) {
+        key = keys[i];
+        if (!reservedKeys[key]) data[key] = args[key];
+    }
+
+    return this.post.create(data, args.r || args.replace).then(function(post) {
+        self.log.info('Created: %s', chalk.magenta(tildify(post.path)));
+    });
 });
 
 hexo.extend.generator.register('showcase', require('./lib/generator'));
